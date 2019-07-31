7448713a2a5e4748000e7bf5f171b1d3c4ddb2f7
Daisy98Faith <daisyfaithauma@gmail.com>
2019-08-01 02:58:47 +0300

    changed the whole body layout

--- pages/poems.php
+++ pages/poems.php
@@ -1,48 +1,60 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Poems</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../custom_css/w3.css">
-    <link rel="stylesheet" href="../custom_css/index.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="../custom_css/poems.css">
+<link rel="stylesheet" href="../custom_css/w3.css">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../custom_css/index.css"> 
+  <link rel="stylesheet" type="text/css" media="screen" href="../custom_css/poems.css">
+  <link href="../fontawesome/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="../flat-icon/flaticon.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.w3-opennav{
+position:fixed;
+top:60px;
+
+}
+.w3-main{
+  margin-left:300px
+}
+.w3-sidenav{
+  width:300px;
+  top:65px;
+  font-size:20px;
+  text-align:center;
+ color:white;
+  list-style-type:none;
+  background:#999999;
+}
+</style>
+ 
 </head>
 
-<body style="background:lightgrey;" data-spy="scroll" data-target="#myScrollspy" data-offset="20">
-   
-   <!-- navigation bar -->
-   <?php
+<body>
+<?php
 include 'navbar.php';
     ?>
-       
-
-    <!-- navigation bar -->
+  <nav class="w3-sidenav w3-text-white w3-collapse  w3-card-2 w3-animate-left" style="">
+    <a href="javascript:void(0)" onclick="w3_close()" 
+    class="w3-closenav w3-large w3-hide-large">Close ×</a>
     <?php
-       echo ' <div class="container  poemRow">
-                 <div class="row ">
-                 <div  class="w3-card-8 w3-red">
-                  <nav class="col-sm-3 col-xs-2" id="myScrollspy">
-                   <ul class="nav nav-pills nav-stacked">';  
-              include '../tmp/connection.php';
-             $poem_sql = "SELECT * FROM poems";
-              $poem_result = $conn->query($poem_sql);
-               if ($poem_result->num_rows > 0) { 
-                   while($poem_row = $poem_result->fetch_assoc()) {
-                    echo ' <li class=""><a href="#'.$poem_row['poem-title'].'">'.$poem_row['poem-title'].'</a></li>';
-                   }
-                  }
-                echo'</ul>
-              </nav>
-              </div>
-              <div class="col-sm-9 col-xs-9 section" >';
-              $poem_sql = "SELECT * FROM poems";
+      include '../tmp/connection.php';    
+      $poem_sql = "SELECT * FROM poems";
+      $poem_result = $conn->query($poem_sql);
+       if ($poem_result->num_rows > 0) { 
+           while($poem_row = $poem_result->fetch_assoc()) {
+            echo ' <li class=""><a href="#'.$poem_row['poem-title'].'">'.$poem_row['poem-title'].'</a></li>';
+           }
+          }
+    ?>		
+  </nav>
+
+  <div class="w3-main w3-white " style="">
+  <span class="w3-opennav w3-btn-floating w3-white w3-xlarge w3-hide-large" style="top:60px;right:20px "onclick="w3_open()">☰</span>
+
+  <?php
+  $poem_sql = "SELECT * FROM poems";
               $poem_result = $conn->query($poem_sql);
               if ($poem_result->num_rows > 0) { 
                 while($poem_row = $poem_result->fetch_assoc()) {
@@ -55,7 +67,7 @@ include 'navbar.php';
                           <img class="poemImage w3-border w3-padding"src="../images/'.$poem_row['image-link'].'" alt="">
                          </div>
                        <h3 class="center">'.$poem_row['poem-title'].'</h3>
-                     <p class="bb" > <pre>'.$poem_row['Poem -Body'].' </pre> </p>  <br>
+                     <p> <pre>'.$poem_row['Poem -Body'].' </pre> </p>  <br>
                       <h4 class="WrittenBy center ">WrittenBy &copy; '.$poem_row['poet'].'</h4>
                       </div>
                       <div class="col-sm-3"></div>
@@ -63,17 +75,23 @@ include 'navbar.php';
                       </div><br>'; 
                 }
                }
-                           
-            echo'  </div>
-            </div>
-          </div>';
-          ?> 
-  <?php
-include 'footer.php';
-    ?>
-    
+
+               
+               include 'footer.php';
+               ?>
+              
+       
+  </div>
+  <script>
+  function w3_open() {
+      document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
+  }
+  function w3_close() {
+      document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
+  }
+  </script>    
     <script src="../js/jquery-3.3.1.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../custom_js/main.js"></script>
-</body>
+  </body>
 </html>
