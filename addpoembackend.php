f9817e150a1bfaa0623776d90d4ab068cabda4c7
Daisy98Faith <daisyfaithauma@gmail.com>
2019-07-31 11:13:52 +0300

    adding un untracked file

---
+++ addpoembackend.php
@@ -0,0 +1,23 @@
+<?php
+ include '../tmp/connection.php';
+ if(isset($_POST['addpoem'])){
+    $poet=$_POST['poet'];
+    $poem_title=$_POST['poemtitle'];
+    if(isset($_SESSION["pic"])){
+    $image= $_SESSION["pic"];
+    }else{
+        $image=null;
+    }
+    $body=$_POST['poembody']; 
+  str_replace(" ' ", " \' ",$body );
+  str_replace(' " ', " \" ",$body );
+ $sql = "INSERT INTO `poems`( `poet`, `poem-title`, `image-link`, `Poem -Body`) VALUES ('$poet','$poem_title','$image','$body')";
+if (mysqli_query($conn, $sql)) {
+    //  echo '("New record created successfully")';
+     session_unset();
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+ }
+$conn->close();
+?>
